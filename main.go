978ea68b4de3f8aca6aa79d778f8fa93870dfb9a
frostyfan109 <groupe@renci.org>
2024-05-04 15:21:13 -0400

    Add/remove collaborators from repo

--- main.go
+++ main.go
@@ -71,6 +71,19 @@ type PatchRepoOptions struct {
 	Private     bool   `json:"private,omitempty"`
 }
 
+type AddCollaboratorOptions struct {
+	Name             string `json:"name"`
+	Owner            string `json:"owner"`
+	CollaboratorName string `json:"collaborator_name"`
+	Permission       string `json:"permission"`
+}
+
+type RemoveCollaboratorOptions struct {
+	Name             string `json:"name"`
+	Owner            string `json:"owner"`
+	CollaboratorName string `json:"collaborator_name"`
+}
+
 type ForkOptions struct {
 	Owner    string `json:"owner"`
 	NewOwner string `json:"newOwner"`
@@ -1424,6 +1437,142 @@ func handleRepo(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func addCollaboratorToRepo(giteaBaseURL, adminUsername, adminPassword, owner, repoName, collaboratorName, permission string) error {
+
+	// Build the Gitea API URL for fetching the repo details
+	url := fmt.Sprintf("%s/repos/%s/%s/collaborators/%s", giteaBaseURL, owner, repoName, collaboratorName)
+
+	// Empty permission string is treated the same as omitting it by the Gitea API here.
+	option := api.AddCollaboratorOption{
+		Permission: &permission,
+	}
+	jsonData, _ := json.Marshal(option)
+
+	// Create a new request
+	req, err := http.NewRequest("PUT", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		log.Printf("Error creating request %v", http.StatusInternalServerError)
+		return err
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.SetBasicAuth(string(adminUsername), string(adminPassword))
+
+	// Send the request
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Printf("Error querying Gitea %v", http.StatusInternalServerError)
+		return fmt.Errorf("HTTP Error: %d", resp.StatusCode)
+	}
+	defer resp.Body.Close()
+
+	// Check if the request was successful
+	if resp.StatusCode != http.StatusNoContent {
+		log.Printf("Error adding contributor from Gitea %v", resp.StatusCode)
+		return fmt.Errorf("HTTP Error: %d", resp.StatusCode)
+	}
+
+	return nil
+}
+
+func handleAddCollaborator(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	if err != nil {
+		http.Error(w, "Failed reading request body", http.StatusInternalServerError)
+		return
+	}
+
+	var options AddCollaboratorOptions
+	err = json.Unmarshal(body, &options)
+	if err != nil {
+		http.Error(w, "Failed parsing request body", http.StatusBadRequest)
+		return
+	}
+
+	// We won't enforce Permission since Gitea doesn't enforce it.
+	if options.Name == "" || options.Owner == "" || options.CollaboratorName == "" {
+		http.Error(w, "Repo name, owner, and collaborator name must be provided", http.StatusBadRequest)
+		return
+	}
+	if err := addCollaboratorToRepo(access.URL, access.Username, access.Password, options.Owner, options.Name, options.CollaboratorName, options.Permission); err == nil {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Contributor added successfully"))
+	} else {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func removeCollaboratorFromRepo(giteaBaseURL, adminUsername, adminPassword, owner, repoName, collaboratorName string) error {
+
+	// Build the Gitea API URL for fetching the repo details
+	url := fmt.Sprintf("%s/repos/%s/%s/collaborators/%s", giteaBaseURL, owner, repoName, collaboratorName)
+
+	// Create a new request
+	req, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		log.Printf("Error creating request %v", http.StatusInternalServerError)
+		return err
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.SetBasicAuth(string(adminUsername), string(adminPassword))
+
+	// Send the request
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Printf("Error querying Gitea %v", http.StatusInternalServerError)
+		return fmt.Errorf("HTTP Error: %d", resp.StatusCode)
+	}
+	defer resp.Body.Close()
+
+	// Check if the request was successful
+	if resp.StatusCode != http.StatusNoContent {
+		log.Printf("Error removing contributor from Gitea %v", resp.StatusCode)
+		return fmt.Errorf("HTTP Error: %d", resp.StatusCode)
+	}
+
+	return nil
+}
+
+func handleRemoveCollaborator(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	if err != nil {
+		http.Error(w, "Failed reading request body", http.StatusInternalServerError)
+		return
+	}
+
+	var options RemoveCollaboratorOptions
+	err = json.Unmarshal(body, &options)
+	if err != nil {
+		http.Error(w, "Failed parsing request body", http.StatusBadRequest)
+		return
+	}
+
+	if options.Name == "" || options.Owner == "" || options.CollaboratorName == "" {
+		http.Error(w, "Repo name, owner, and collaborator name must be provided", http.StatusBadRequest)
+		return
+	}
+	if err := removeCollaboratorFromRepo(access.URL, access.Username, access.Password, options.Owner, options.Name, options.CollaboratorName); err == nil {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Contributor removed successfully"))
+	} else {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func handleRepoCollaborator(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPut:
+		handleAddCollaborator(w, r)
+	case http.MethodDelete:
+		handleRemoveCollaborator(w, r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
 func forkRepositoryForUser(giteaBaseURL, adminUsername, adminPassword, owner, repo, user string) (*api.Repository, error) {
 	/*
 		reenable this once gitea bug #26234 is fixed
@@ -1772,6 +1921,7 @@ func main() {
 	r.HandleFunc("/onPush", webhookHandler)
 	r.HandleFunc("/users", handleUser)
 	r.HandleFunc("/repos", handleRepo)
+	r.HandleFunc("/repos/collaborators", handleRepoCollaborator)
 	r.HandleFunc("/repos/download", handleDownloadRepo).Methods("GET")
 	r.HandleFunc("/forks", handleFork)
 	r.HandleFunc("/orgs", handleOrg)
