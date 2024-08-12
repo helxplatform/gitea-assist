7018667afa37f96812723b0e41bf6e8ce3b580a1
Hina Shah <hinashah@renci.org>
2024-08-12 12:44:24 -0400

    BUG: fix server setup

--- main.go
+++ main.go
@@ -2434,7 +2434,7 @@ func main() {
 
 	srv := &http.Server{
 		Handler:      router,
-		Addr:         "127.0.0.1:9000",
+		Addr:         "0.0.0.0:9000",
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
